0038b0517b06fd66a64cf0b627fc01dfb3b36246
Roshan <165517367+ROSHAN-Z89@users.noreply.github.com>
2025-07-26 08:52:06 +0530

    Update enabling_camera.py

--- enabling_camera.py
+++ enabling_camera.py
@@ -1,8 +1,8 @@
 import cv2
 video_cap = cv2.VideoCapture(0)
 while True :
-    ret ,vid = video_cap.read()
-    cv2.imshow("video_live",vid)
+    ret ,video = video_cap.read()
+    cv2.imshow("video_live",video)
     if cv2.waitKey(10) == ord('a'):
         break
 video_cap.release()
